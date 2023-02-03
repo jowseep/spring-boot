192a1affc10f2255c5380eb62400b9d637ca83df
jowseep <jowseep@gmail.com>
2023-02-03 17:02:51 +0800

    added get, post, and delete function

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -1,9 +1,22 @@
 package com.example.demo;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
 public class Customer {
 
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
     private String name;
     private String emailAddress;
--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -2,71 +2,43 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-    @GetMapping("/")
-    public greetResponse greet() {
-        greetResponse gr = new greetResponse("Hey", List.of("Java","Golang","JavaScript"), new Person("Joseph", 24, 500000));
-        return gr;
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
     }
 
-    record greetResponse(String greet,
-                         List<String> listOfProgrammingLanguages,
-                         Person person) {};
+    record NewCustomerRequest(String emailAddress, String name, Integer age) {};
 
-    class Person {
-        private String name;
-        private int age;
-        private double savings;
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setEmailAddress(request.emailAddress());
+        customer.setName(request.name());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
 
-        Person(String name, int age, double savings) {
-            this.name = name;
-            this.age = age;
-            this.savings = savings;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public double getSavings() {
-            return savings;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Person person = (Person) o;
-            return age == person.age && Double.compare(person.savings, savings) == 0 && Objects.equals(name, person.name);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(name, age, savings);
-        }
-
-        @Override
-        public String toString() {
-            return "Person{" +
-                    "name='" + name + '\'' +
-                    ", age=" + age +
-                    ", savings=" + savings +
-                    '}';
-        }
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable ("customerId") Integer id) {
+        customerRepository.deleteById(id);
     }
 }
